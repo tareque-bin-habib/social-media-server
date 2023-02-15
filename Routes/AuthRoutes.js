99353883438db39757552e81b760a03230599029
tareque-bin-habib <shad.tbh@gmail.com>
2023-02-15 19:42:55 +0600

    login function done

--- Routes/AuthRoutes.js
+++ Routes/AuthRoutes.js
@@ -1,10 +1,10 @@
 import express from 'express'
+import { loginUser, registerUser } from '../Controllers/AuthController.js'
 
 const router = express.Router()
 
-router.get('/', async (req, res) => {
-    res.send('Authentication route')
-})
+router.post('/register', registerUser)
+router.post('/login', loginUser)
 
 
 export default router
