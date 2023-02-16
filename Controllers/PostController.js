ae052d868dff2cf685c7efef8204f3c7efb31316
tareque-bin-habib <shad.tbh@gmail.com>
2023-02-16 16:51:16 +0600

    like dislike api done

--- Controllers/PostController.js
+++ Controllers/PostController.js
@@ -65,3 +65,26 @@ export const deletePost = async (req, res) => {
         res.status(500).json(error)
     }
 }
+
+// Like and Dislike a post
+export const likePost = async (req, res) => {
+    const id = req.params.id
+    const { userId } = req.body
+
+    try {
+
+        const post = await PostModel.findById(id)
+
+        if (!post.likes.includes(userId)) {
+            await post.updateOne({ $push: { likes: userId } })
+            res.status(200).json("Post liked")
+        }
+        else {
+            await post.updateOne({ $pull: { likes: userId } })
+            res.status(200).json("Post unliked")
+        }
+
+    } catch (error) {
+        res.status(500).json(error)
+    }
+}
--- Routes/PostRoute.js
+++ Routes/PostRoute.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { createPost, deletePost, getPost, updatePost } from '../Controllers/PostController.js'
+import { createPost, deletePost, getPost, likePost, updatePost } from '../Controllers/PostController.js'
 
 const router = express.Router()
 
@@ -7,5 +7,6 @@ router.post('/', createPost)
 router.get('/:id', getPost)
 router.put('/:id', updatePost)
 router.delete('/:id', deletePost)
+router.put('/:id/like', likePost)
 
 export default router
