572b869208cbdf61f3e50806320a6c56ffb5ae8b
tareque-bin-habib <shad.tbh@gmail.com>
2023-02-16 17:20:51 +0600

    all api created

--- Controllers/PostController.js
+++ Controllers/PostController.js
@@ -1,5 +1,6 @@
 import PostModel from "../Models/postModel.js";
 import mongoose from 'mongoose'
+import UserModel from "../Models/userModel.js";
 
 // create new post
 
@@ -83,6 +84,45 @@ export const likePost = async (req, res) => {
             await post.updateOne({ $pull: { likes: userId } })
             res.status(200).json("Post unliked")
         }
+
+    } catch (error) {
+        res.status(500).json(error)
+    }
+}
+
+// get Timeline post
+export const getTimelinePost = async (req, res) => {
+    const userId = req.params.id
+
+    try {
+
+        const currentUserPosts = await PostModel.find({ userId: userId })
+        const followingPosts = await UserModel.aggregate([
+            {
+                $match: {
+                    _id: new mongoose.Types.ObjectId(userId)
+                }
+            },
+            {
+                $lookup: {
+                    from: "posts",
+                    localField: "following",
+                    foreignField: "userId",
+                    as: "followingPosts"
+                }
+            },
+            {
+                $project: {
+                    followingPosts: 1,
+                    _id: 0
+                }
+            }
+        ])
+
+        res.status(200).json(currentUserPosts.concat(...followingPosts[0].followingPosts).sort((a, b) => {
+            return b.createdAt - a.createdAt
+        }))
+
 
     } catch (error) {
         res.status(500).json(error)
--- Routes/PostRoute.js
+++ Routes/PostRoute.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { createPost, deletePost, getPost, likePost, updatePost } from '../Controllers/PostController.js'
+import { createPost, deletePost, getPost, getTimelinePost, likePost, updatePost } from '../Controllers/PostController.js'
 
 const router = express.Router()
 
@@ -8,5 +8,6 @@ router.get('/:id', getPost)
 router.put('/:id', updatePost)
 router.delete('/:id', deletePost)
 router.put('/:id/like', likePost)
+router.get('/:id/timeline', getTimelinePost)
 
 export default router
