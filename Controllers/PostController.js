a230cdc8db4236b1f8b7ae51448fd811000a896f
tareque-bin-habib <shad.tbh@gmail.com>
2023-02-16 16:37:31 +0600

    delete post api done

--- Controllers/PostController.js
+++ Controllers/PostController.js
@@ -44,3 +44,24 @@ export const updatePost = async (req, res) => {
         res.status(500).json(error)
     }
 }
+
+// Delete a post
+export const deletePost = async (req, res) => {
+    const id = req.params.id
+    const { userId } = req.body
+
+    try {
+        const post = await PostModel.findById(id)
+
+        if (post.userId === userId) {
+            await post.deleteOne()
+            res.status(200).json("Post Deleted successfully")
+        }
+        else {
+            res.status(403).json("Action forbidden")
+        }
+
+    } catch (error) {
+        res.status(500).json(error)
+    }
+}
