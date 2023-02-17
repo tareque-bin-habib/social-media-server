4b71473146cb959c47fd8584795213d608902b72
tareque-bin-habib <shad.tbh@gmail.com>
2023-02-17 19:04:03 +0600

    checking

--- Controllers/AuthController.js
+++ Controllers/AuthController.js
@@ -1,18 +1,31 @@
 import UserModel from '../Models/userModel.js'
 import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
 
 // Registr a new user
 export const registerUser = async (req, res) => {
-    const { username, password, firstname, lastname } = req.body
+
 
     const salt = await bcrypt.genSalt(10)
-    const hashedPass = await bcrypt.hash(password, salt)
-
-    const newUser = new UserModel({ username, password: hashedPass, firstname, lastname })
+    const hashedPass = await bcrypt.hash(req.body.password, salt)
+    req.body.password = hashedPass
+    const newUser = new UserModel(req.body)
+    const { username } = req.body
 
     try {
-        await newUser.save();
-        res.status(200).json(newUser)
+
+
+        const oldUser = await UserModel.findOne({ username })
+
+        if (oldUser) {
+            return res.status(400).json({ message: "username is already registered" })
+        }
+
+        const user = await newUser.save();
+        const token = jwt.sign({
+            username: user.username, id: user._id
+        }, process.env.JWT_KEY, { expiresIn: '1h' })
+        res.status(200).json({ user, token })
     } catch (error) {
         res.status(500).json({ message: error.message })
     }
@@ -28,7 +41,15 @@ export const loginUser = async (req, res) => {
         if (user) {
             const validity = await bcrypt.compare(password, user.password)
 
-            validity ? res.status(200).json(user) : res.status(400).json("Wrong password")
+            if (!validity) {
+                res.status(400).json("Wrong password")
+            }
+            else {
+                const token = jwt.sign({
+                    username: user.username, id: user._id
+                }, process.env.JWT_KEY, { expiresIn: '1h' })
+                res.status(200).json({ user, token })
+            }
         }
         else {
             res.status(404).json("User does not exists")
--- index.js
+++ index.js
@@ -2,18 +2,25 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose'
 import dotenv from 'dotenv'
+import cors from 'cors'
 import AuthRoutes from './Routes/AuthRoutes.js'
 import UserRoute from './Routes/UserRoute.js'
 import PostRoute from './Routes/PostRoute.js'
+import UploadRoute from './Routes/UploadRoute.js'
 
 // Routes
 
 
 const app = express()
 
+// To save images to public
+app.use(express.static('public'))
+app.use('/images', express.static("images"))
+
 // Middleware
 app.use(bodyParser.json({ limit: '30mb', extended: true }))
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
+app.use(cors())
 
 
 dotenv.config()
@@ -28,3 +35,6 @@ mongoose.connect(process.env.MONGO_DB, { useNewUrlParser: true, useUnifiedTopolo
 app.use('/auth', AuthRoutes)
 app.use('/user', UserRoute)
 app.use('/post', PostRoute)
+app.use('/upload', UploadRoute)
+
+console.log('da')
